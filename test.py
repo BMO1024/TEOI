e6459583ca72340a62a550a8178705158bd65f4e
LHeisenberg <71958794+BMO1024@users.noreply.github.com>
2023-05-01 02:03:45 +0800

    愚蠢的AI

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from test_functions.test_Trigonometric_Integrals import test_coshint, test_ei, test_expint, test_sinhint, test_eulergamma, test_cosint, test_sinint, test_logint, test_ssi
-
+from test_functions.test_Elliptic_Integrals import test_ellipke
 import numpy as np
 import scipy
     
---
+++ test_functions/test_Elliptic_Integrals.py
@@ -0,0 +1,8 @@
+from Elliptic_Integrals.ellipke import 
+
+import numpy as np
+import scipy
+
+def test_ellipke():
+
+    
