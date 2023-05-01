189649ce0f1452349e931e43ef039f1d87e2282b
LHeisenberg <71958794+BMO1024@users.noreply.github.com>
2023-05-02 01:18:06 +0800

    完成ellipticK，进度13/18

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from test_functions.test_Trigonometric_Integrals import test_coshint, test_ei, test_expint, test_sinhint, test_eulergamma, test_cosint, test_sinint, test_logint, test_ssi
-from test_functions.test_Elliptic_Integrals import test_ellipke, test_ellipticCE, test_ellipticCK, test_ellipticCPi
+from test_functions.test_Elliptic_Integrals import test_ellipke, test_ellipticCE, test_ellipticCK, test_ellipticCPi, test_ellipticK
 import numpy as np
 import scipy
     
@@ -20,4 +20,5 @@ if __name__ == '__main__':
     # test_ellipke()
     # test_ellipticCE()
     # test_ellipticCK()
-    test_ellipticCPi()
+    # test_ellipticCPi()
+    test_ellipticK()
--- test_functions/test_Elliptic_Integrals.py
+++ test_functions/test_Elliptic_Integrals.py
@@ -3,6 +3,7 @@ from Elliptic_Integrals.ellipke import elliptic_integral_second_kind
 from Elliptic_Integrals.ellipticCE import elliptic_integral_second_kind_CE
 from Elliptic_Integrals.ellipticCK import elliptic_integral_first_kind_CK
 from Elliptic_Integrals.ellipticCPi import elliptic_integral_third_kind_CPi
+from Elliptic_Integrals.ellipticK import ellipticK
 
 import numpy as np
 import scipy
@@ -58,3 +59,14 @@ def test_ellipticCPi():
     expected_y = np.array([1.3703, 1.8541, 4.9673, np.inf])
     print("expected_y = ", expected_y)
     assert np.allclose(actual_y, expected_y, rtol=1e-03, atol=1e-08, equal_nan=False)
+
+def test_ellipticK():
+    
+        m = np.array([0, 1/2, 1/4, 1])
+    
+        actual_y = ellipticK(m)
+        print("actual_y = ", actual_y)
+    
+        expected_y = scipy.special.ellipk(m)
+        print("expected_y = ", expected_y)
+        assert np.allclose(actual_y, expected_y, rtol=1e-05, atol=1e-08, equal_nan=False)
